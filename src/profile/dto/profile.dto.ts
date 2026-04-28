0aad6cd391c22e761d546330eb0fc4ee0af1ce3f
Oluwapelumi Kehinde <pelumiwork@gmail.com>
2026-04-28 15:31:15 +0100

    fix: add validation decorators to profile DTOs

--- src/profile/dto/profile.dto.ts
+++ src/profile/dto/profile.dto.ts
@@ -1,66 +1,106 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { IsString, IsOptional, IsNotEmpty, IsEmail, MinLength } from 'class-validator';
 
 export class UpdatePersonalInfoDto {
     @ApiProperty({ required: false })
+    @IsString()
+    @IsOptional()
     dateOfBirth?: string;
 
     @ApiProperty({ required: false })
+    @IsString()
+    @IsOptional()
     gender?: string;
 
     @ApiProperty({ required: false })
+    @IsString()
+    @IsOptional()
     bloodGroup?: string;
 
     @ApiProperty({ required: false })
+    @IsString()
+    @IsOptional()
     genotype?: string;
 
     @ApiProperty({ required: false })
+    @IsString()
+    @IsOptional()
     medicalCondition?: string;
 
     @ApiProperty({ required: false })
+    @IsString()
+    @IsOptional()
     allergies?: string;
 }
 
 export class UpdateContactInfoDto {
     @ApiProperty({ required: false })
+    @IsString()
+    @IsOptional()
     phoneNumber?: string;
 
     @ApiProperty({ required: false })
+    @IsString()
+    @IsOptional()
     residentialAddress?: string;
 
     @ApiProperty({ required: false })
+    @IsString()
+    @IsOptional()
     stateOfOrigin?: string;
 
     @ApiProperty({ required: false })
+    @IsString()
+    @IsOptional()
     lga?: string;
 
     @ApiProperty({ required: false })
+    @IsString()
+    @IsOptional()
     nationality?: string;
 }
 
 export class UpdateGuardianInfoDto {
     @ApiProperty({ required: false })
+    @IsString()
+    @IsOptional()
     fullName?: string;
 
     @ApiProperty({ required: false })
+    @IsString()
+    @IsOptional()
     relationship?: string;
 
     @ApiProperty({ required: false })
+    @IsString()
+    @IsOptional()
     phoneNumber?: string;
 
     @ApiProperty({ required: false })
+    @IsEmail()
+    @IsOptional()
     email?: string;
 
     @ApiProperty({ required: false })
+    @IsString()
+    @IsOptional()
     occupation?: string;
 
     @ApiProperty({ required: false })
+    @IsString()
+    @IsOptional()
     workAddress?: string;
 }
 
 export class UpdatePasswordDto {
     @ApiProperty()
+    @IsString()
+    @IsNotEmpty()
     oldPassword: string;
 
     @ApiProperty()
+    @IsString()
+    @IsNotEmpty()
+    @MinLength(8)
     newPassword: string;
 }
