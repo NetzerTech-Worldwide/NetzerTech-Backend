f7b14625136daebf46a630e989322a25c8bc6721
Oluwapelumi Kehinde <pelumiwork@gmail.com>
2026-04-28 15:29:47 +0100

    fix: add validation decorators to CreateStudentWithParentDto to allow whitelisting

--- src/admin/dto/admin.dto.ts
+++ src/admin/dto/admin.dto.ts
@@ -1,4 +1,5 @@
 import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
+import { IsString, IsEmail, IsOptional, IsNotEmpty } from 'class-validator';
 
 export class AdminClassOverviewDto {
     @ApiProperty()
@@ -109,45 +110,73 @@ export class AdminParentDto {
 
 export class CreateStudentWithParentDto {
     @ApiProperty()
+    @IsString()
+    @IsNotEmpty()
     studentFirstName: string;
 
     @ApiProperty()
+    @IsString()
+    @IsNotEmpty()
     studentLastName: string;
 
     @ApiProperty()
+    @IsString()
+    @IsNotEmpty()
     gender: string;
 
     @ApiProperty()
+    @IsString()
+    @IsNotEmpty()
     dateOfBirth: string;
 
     @ApiProperty()
+    @IsString()
+    @IsNotEmpty()
     class: string;
 
     @ApiPropertyOptional()
+    @IsEmail()
+    @IsOptional()
     studentEmail?: string;
 
     @ApiProperty()
+    @IsString()
+    @IsNotEmpty()
     parentTitle: string;
 
     @ApiProperty()
+    @IsString()
+    @IsNotEmpty()
     parentFirstName: string;
 
     @ApiProperty()
+    @IsString()
+    @IsNotEmpty()
     parentLastName: string;
 
     @ApiProperty()
+    @IsString()
+    @IsNotEmpty()
     parentPhone: string;
 
     @ApiProperty()
+    @IsEmail()
+    @IsNotEmpty()
     parentEmail: string;
 
     @ApiPropertyOptional()
+    @IsString()
+    @IsOptional()
     parentOccupation?: string;
 
     @ApiPropertyOptional()
+    @IsString()
+    @IsOptional()
     parentAddress?: string;
 
     @ApiProperty()
+    @IsString()
+    @IsNotEmpty()
     relationship: string;
 }
 
